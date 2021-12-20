e7d7cdaba3332cbeb601dbce1d4db83dd0067f93
ivan-kononenko <59476916+ivan-kononenko@users.noreply.github.com>
2021-12-20 20:15:15 +0200

    finished

--- lesson_3.py
+++ lesson_3.py
@@ -2,12 +2,10 @@ import math
 
 
 class Shape:
-    center = (0, 0)
-
-    def __init__(self, center):
+    def __init__(self, center=(0, 0)):
         self.center = center
 
-    def get_distance(figure_1, figure_2):
+    def get_distance(self, figure1, figure2):
         return math.sqrt(
                 (figure2.center[0] - figure1.center[0])**2 +
                 (figure2.center[1] - figure1.center[1])**2
@@ -24,7 +22,9 @@ class Circle(Shape):
 
     def get_area(self):
         return math.pi*self.radius**2
-
+    
+    def move(self, x,y):
+        center = (x, y)
 
 class Square(Shape):
     def __init__(self, center=(0, 0), side=1):
@@ -42,7 +42,8 @@ class Square(Shape):
                (self.center[0] - self.side/2, self.center[1] + self.side/2),\
                (self.center[0] + self.side/2, self.center[1] + self.side/2),\
                (self.center[0] + self.side/2, self.center[1] - self.side/2)
-
+    def move(self, x,y):
+        center = (x, y)
 
 class Triangle(Shape):
     def __init__(self, center=(0, 0), side=1):
@@ -61,5 +62,27 @@ class Triangle(Shape):
                (self.center[0], self.center[1] + h * 2/3),\
                (self.center[0] + self.side/2, self.center[1] - h * 1/3)
 
+    def move(self, x,y):
+        center = (x, y)
 
 
+shape = Shape()
+c1 = Circle()
+s1 = Square((3, 3), 3)
+t1 = Triangle((1, 0), 2)
+
+
+c1.get_center()
+c1.get_area()
+
+s1.get_center()
+s1.get_area()
+s1.get_vertex()
+s1.move(10, 13)
+
+t1.get_center()
+t1.get_area()
+t1.get_vertex()
+t1.move(-42, 69)
+
+shape.get_distance(s1, t1)
